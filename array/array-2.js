18326c5dd429e884e36d782928db861a55c07400
Luciascarevnea <141555242+Luciascarevnea@users.noreply.github.com>
2024-02-24 16:20:41 +0300

    Add files via upload

---
+++ array/array-2.js
@@ -0,0 +1,18 @@
+// 1. Sa utilizam typeof-> el ne arata tipul variabilei
+// 2. console.log(names)
+// 3. console.table(name)
+// 4. for(let i=0; i<3; i++){
+//     console.log(names[i])
+//}
+
+// let n = 100  number
+// let b = true boolean
+// let s = "ABC" string
+// let a = [1, 2. 3] array
+
+// MEMORY
+// [        VALUE   ]
+// [    +PROPERTIES ]
+// [    + methods() ]
+
+// n.toFixed-> aceasta functie rescrie nr. cu precizie
